a9cd4dd156857b9f2a8ae4f4716c13f91d8dabea
wenheng <wenheng@163.cn>
2018-12-19 18:14:44 +0800

    第一次提交

---
+++ src/main/java/cn/itcast/SpringbootDemo1MybatisApplication.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo1MybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemo1MybatisApplication.class, args);
+	}
+
+}
+
